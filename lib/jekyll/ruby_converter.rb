707d634215a5cadeed9b96387b9cfe0fa01df78a
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-07-16 22:27:50 +0900

    Initial commit

---
+++ lib/jekyll/ruby_converter.rb
@@ -0,0 +1,7 @@
+require "jekyll/ruby_converter/version"
+
+module Jekyll
+  module RubyConverter
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/ruby_converter/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module RubyConverter
+    VERSION = "0.1.0"
+  end
+end
